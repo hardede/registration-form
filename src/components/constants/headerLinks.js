3a802a806ee4997ee958c376e893a8abae01e241
Hardede <hardede@MacBook-Pro-aaa.local>
2023-02-12 18:25:50 +0200

    fix 2

--- src/components/constants/headerLinks.js
+++ src/components/constants/headerLinks.js
@@ -1,7 +1,7 @@
 import { v4 as uuidv4 } from "uuid";
 import HowToPlay from "../../assets/howtoplat.svg";
 import Rules from "../../assets/rules.svg";
-import Dashboard from "../../assets/icon.svg";
+import Dashboard from "../../assets/dashboard.svg";
 
 export const headerLinks = [
   {
